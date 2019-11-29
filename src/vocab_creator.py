aa92c9798ea1c3012fdd7a656c96929d5ead125c
Anirudh Swaminathan <aniswami97@gmail.com>
2019-11-28 23:41:11 -0800

    Save vocabulary
    Also create a word to index map useful for RNN

--- src/vocab_creator.py
+++ src/vocab_creator.py
@@ -5,22 +5,44 @@ from pycocotools.coco import COCO
 from collections import Counter
 import re
 
+import os
+import numpy as np
+
 
 class VocabCreate(object):
     """Class to create the vocabulary for our project"""
 
-    def __init__(self, data_pth):
+    def __init__(self, data_pth, output_path):
         """Constructor
         """
+        if os.path.exists(output_path):
+            self.one_hot_inds = np.load(output_path)
+            return
+
+        # the path to the json file containing the annotations
         self.data_pth = data_pth
+
+        # data holds the dictionary corresponding to the JSON file
         self.data = None
+
+        # dict contains the final word list with words of frequency >= threshold
         self.dict = None
+
+        # One hot indices required for embedded layer of RNN
+        self.one_hot_inds = dict()
+
+        # words and its frequency in the original dataset
         self.vocab_list = Counter()
         self.load_data()
         self.frequency_list()
         self.remove_lowest(thresh=5)
         # print(len(self.vocab_list.keys()))
-        vals = [self.vocab_list[v] for v in self.dict]
+        # vals = [self.vocab_list[v] for v in self.dict]
+        self.create_inds()
+
+        # Creates a list of tuples
+        self.one_hot_inds = np.array(list(self.one_hot_inds.items()))
+        np.save(output_path, self.one_hot_inds)
         # print(set(vals), len(vals))
 
     def load_data(self):
@@ -48,9 +70,24 @@ class VocabCreate(object):
         ret = [w for w in self.vocab_list.keys() if self.vocab_list[w] >= thresh]
         self.dict = ret
 
+    def create_inds(self):
+        """
+        Creates a dictionary with the index returned for each word
+        :return:
+        """
+        # add start and end vectors to the vocabulary
+        start_vec = "start_vec"
+        end_vec = "end_vec"
+        self.dict.append(start_vec)
+        self.dict.append(end_vec)
+
+        # Generate distinct indices for each word in the vocabulary
+        for i in range(len(self.dict)):
+            self.one_hot_inds[self.dict[i]] = i
+
 
 def main():
-    vocab = VocabCreate("../datasets/COCO/annotations/captions_train2014.json")
+    vocab = VocabCreate("../datasets/COCO/annotations/captions_train2014.json", "../outputs/vocab.npy")
     print("Loaded successfully!")
     print("There are ", len(vocab), " unique words with frequency >=5 in the dataset")
 
